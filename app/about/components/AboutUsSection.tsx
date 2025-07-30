980513d0ec0883bb7bcd8f9244f0cf2ddd5d4495
hammad-datapulse <hahmed@datapulsetechnologies.org>
2025-07-31 00:01:57 +0500

    Alot of deletions + finaling

--- app/about/components/AboutUsSection.tsx
+++ app/about/components/AboutUsSection.tsx
@@ -35,7 +35,7 @@ export default function AboutUsSection() {
         </div>
         <PrimaryButton
           text="Join Our Mission"
-          onClick={() => window.location.href = "/register"}
+          onClick={() => window.location.href = "/production"}
           className="mt-16 text-white px-10 py-4 rounded-full"
           disabled={false}
           />
